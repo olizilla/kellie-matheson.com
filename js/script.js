3551e84144e4f27f1036a95fb78c580b7290c7fc
olizilla <oli@zilla.org.uk>
2012-01-26 11:41:04 +0000

    Fluid layout, gratuitous image fade in on scroll

--- js/script.js
+++ js/script.js
@@ -1,26 +1,20 @@
-/* Author: 
-
+/* Author: Oli Evans <oli@zilla.org.uk>
 */
 
+$('.lazy img').hide();
 
+$('.lazy article').appear(function(){
+	$('img', $(this)).fadeIn('slow');
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//$('article').appear(function(){
+//	var lazy = $('.lazy', $(this));
+//	var src = lazy.attr('data-src');
+//	var img = $('<img/>').hide();
+//	img.attr('src', src);
+//	lazy.replaceWith(img);
+//	img.ready(function(){
+//		img.fadeIn('slow');
+//	})
+//});
 
